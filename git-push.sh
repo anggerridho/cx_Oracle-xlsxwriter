ff3925e2729e86b03655fbf8d9a962ec26c944ad
Angger Ridho Abdullah <anggeridho@gmail.com>
2021-03-26 18:43:42 +0700

    RBT_Daily_Report

--- git-push.sh
+++ git-push.sh
@@ -5,3 +5,7 @@ git add .
 git commit -m "RBT_Daily_Report"
 git status
 git push -u origin anggerridho
+
+# Note if error
+# https://stackoverflow.com/questions/24114676/git-error-failed-to-push-some-refs-to-remote
+#git pull --rebase
