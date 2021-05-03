5e6e366ec7b9e814a2d159387d45dd1f2a12a287
anggerridho <anggeridho@gmail.com>
2021-05-04 00:32:54 +0700

    RBT_Daily_Report

--- git-push.sh
+++ git-push.sh
@@ -1,10 +1,10 @@
 cd /root/Daily/
-git status
+#git status
 #git add RBT_Daily_Report.ipynb
-git add .
-git commit -m "RBT_Daily_Report"
-git status
-git push -u origin anggerridho
+#git add .
+#git commit -m "RBT_Daily_Report"
+#git status
+#git push -u origin anggerridho
 
 # Note if error
 # https://stackoverflow.com/questions/24114676/git-error-failed-to-push-some-refs-to-remote
